a17c3dcb908189d78301cdb090c48f9213845628
mackattack3k <marcushanssonmh@gmail.com>
2020-05-17 17:43:28 +0200

    Basename for router

--- src/App.tsx
+++ src/App.tsx
@@ -181,7 +181,7 @@ function App() {
 
     return (
         <div className="App">
-            <Router>
+            <Router basename={process.env.PUBLIC_URL}>
                 <Switch>
                     <Route exact path="/auth" component={Auth} />
                     <Route path="/" component={Home} />
