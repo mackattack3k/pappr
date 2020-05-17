d0268c0f705000952838c06b432057aed34c9bea
mackattack3k <marcushanssonmh@gmail.com>
2020-05-17 13:37:52 +0200

    Updates

---
+++ precache-manifest.96a2c202db42e09c5cf88a684588a274.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc83ecf34a392ad5a06b22a95901880c",
+    "url": "/pappr/index.html"
+  },
+  {
+    "revision": "6e85f5be520e4196dc20",
+    "url": "/pappr/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e6385298d2895e85f96a",
+    "url": "/pappr/static/js/2.36a73fa1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pappr/static/js/2.36a73fa1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e85f5be520e4196dc20",
+    "url": "/pappr/static/js/main.467d88c3.chunk.js"
+  },
+  {
+    "revision": "b747aebd0fef40323e75",
+    "url": "/pappr/static/js/runtime-main.6b294b58.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pappr/static/media/logo.5d5d9eef.svg"
+  }
+]);
