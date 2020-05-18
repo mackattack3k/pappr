18adfedcaffad632e70f313ca4a370faebfd66e5
mackattack3k <marcushanssonmh@gmail.com>
2020-05-18 22:19:52 +0200

    Redirect to locatio npath

--- src/config.ts
+++ src/config.ts
@@ -6,9 +6,6 @@ export const CLIENT_ID =
   process.env.NODE_ENV === "production"
     ? "NxTWOwoh3zE_GA"
     : process.env.REACT_APP_REDDIT_CLIENT_ID;
-export const REDIRECT_URL =
-  process.env.NODE_ENV === "production"
-    ? "https://mackattack3k.github.io/pappr/auth"
-    : "http://localhost:3000/auth";
+export const REDIRECT_URL = `${window.location.href}/auth`;
 export const authUrl = `https://www.reddit.com/api/v1/authorize?client_id=${CLIENT_ID}&response_type=code&duration=permanent&state=RANDOM_STRING&scope=read&redirect_uri=${REDIRECT_URL}`;
 export const API_URL = `https://oauth.reddit.com/`;
